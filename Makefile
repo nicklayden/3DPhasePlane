fea6879e464eaf73daa1351478a1e4fd27f956a8
Nick <nicholas.layden@gmail.com>
2018-02-22 20:28:11 -0400

    Added makefile and config.ini in main folder

---
+++ Makefile
@@ -0,0 +1,53 @@
+
+#compiler
+cxx = g++
+
+# executable name
+program = run
+
+# source files
+srcs = minimal-cplane.cpp
+
+# directory to sources and compiled obj files
+srcdir = src
+objdir = obj
+
+objs = $(srcs:.cpp=.o)
+bins = $(src:.cpp=)
+
+# global cpp flags
+cppflags = -std=c++11 -O3
+
+# specialized cpp flags - per compile basis
+cppflags += $(cppflags-$@)
+# flags needed only to compile executable at final step
+cppflags-run += -lGLU -lGL -lglut -lboost_program_options -lsfml-graphics -lsfml-system -lsfml-window
+
+# linker flags:
+#linkflags = -lGLU -lGL -lglut -lboost_program_options
+
+
+# giving folder prefix to the executable files.
+exeobjs = $(addprefix $(objdir)/, $(objs))
+
+# recipe to make
+all: $(program)
+
+$(program): $(exeobjs)
+	$(cxx) -o $(program) $(exeobjs) $(cppflags) $(linkflags)
+
+$(objdir)/%.o: $(srcdir)/%.cpp | objdirmk
+	$(cxx) $(cppflags) -c $< -o $@
+
+
+# check to see if obj/ is already a directory. if not, make it.
+objdirmk:
+	@mkdir -p $(objdir)
+
+.PHONY: clean
+# removes the executable and the compiled obj .o files
+clean:
+	@echo Removing executable and cleaning object directories.
+	$(RM) $(program) $(EXEOBJS) 
+	$(RM) -r $(objdir)
+
--- src/minimal-cplane.cpp
+++ src/minimal-cplane.cpp
@@ -8,6 +8,9 @@
 
 
     There are a considerable number of optimizations to be done, so just compile with -O3
+
+
+    SCROLL DOWN 
 */
 #include <GL/glut.h>
 #include <SFML/Graphics.hpp>
@@ -15,6 +18,7 @@
 #include <math.h>
 #include <random>
 #include <iostream>
+#include <map>
 
 #include <boost/numeric/odeint/integrate/integrate_const.hpp>
 #include <boost/numeric/odeint.hpp>
@@ -31,9 +35,14 @@ inline double x_start(double y) {
     return sqrt(1 - y*y);
 }
 
-double random(double a, double b) {
+inline double decel(double x, double y) {
+    return 2*x*x - y*y;
+}
+
+inline double random(double a, double b) {
     std::random_device rd;
     std::mt19937 eng(rd());
+    // how to seed:
     // std::mt19937 eng;
     // eng.seed(1993);
     std::uniform_real_distribution<double> distr(a,b);
@@ -54,6 +63,23 @@ double generate_random_circle(std::vector<double>& xin, std::vector<double>& yin
 
 }
 
+double generate_random_annulus(std::vector<double>& xin, std::vector<double>& yin, double a, double b, double radius, double thetamax, int N) {
+    double r, theta, xt, yt;
+    for (int i=0; i < N; i++) {
+        r = random(0.,radius*radius);
+        theta = random(0.,thetamax);
+        xt = sqrt(r)*cos(theta) + a;
+        yt = sqrt(r)*sin(theta) + b;
+        xin.push_back(xt);
+        yin.push_back(yt);
+    }
+
+}
+
+double generate_random_sphere(std::vector<double>& xin, std::vector<double>& yin, double a, double b, double radius, double thetamax, int N) {
+    return 0; // on windows computer implementation
+}
+
 struct push_back_state_and_time 
 {
     std::vector<std::vector<double> >& m_states;
@@ -71,11 +97,17 @@ struct push_back_state_and_time
 
 };
 
-class sean_problem
+/****************************************************************************
+ * 
+ *  CHANGE ONLY THE VARIABLES IN THIS DYNAMICAL SYSTEM 
+ * 
+ * **************************************************************************/
+
+class dynamical_system
 {
 public:
     double m_par;
-    sean_problem(double par): m_par(par) {}
+    dynamical_system(double par): m_par(par) {}
     // exponential potential with constant epsilon background
 
     void operator() (const std::vector<double>& xin, std::vector<double>& dxdt, const double /* t */) {
@@ -83,21 +115,43 @@ public:
         double c = sqrt(6.)/2.;
         x = xin[0]; y = xin[1]; z = xin[2];
     
-        dxdt[0] = x*(-2 + 2*x*x - y*y - z*z) + c*m_par*z*z;
-        dxdt[1] = y*(1 + 2*x*x - y*y - z*z);
-        dxdt[2] = z*(-c*m_par*x + 2*x*x + 1 - y*y - z*z);
+        // dxdt[0] = x*(-2 + 2*x*x - y*y - z*z) + c*m_par*z*z;
+        // dxdt[1] = y*(1 + 2*x*x - y*y - z*z);
+        // dxdt[2] = z*(-c*m_par*x + 2*x*x + 1 - y*y - z*z);
+
+        // dxdt[0] =  -2*x -  (x*x + y*y);
+        // dxdt[1] = -(1.-x)*y;
+        // dxdt[2] = 0;
+
+        // dxdt[0] = -3*x + 3*x*x - m_par*y*(1/(sqrt(x*x + y*y)));
+        // dxdt[1] = m_par*x*(1/(sqrt(x*x + y*y)));
+        // dxdt[2] = 0;
+        // Harmonic potential equations.
+        dxdt[0] = -3*x - z*y - x*(y*y - 2*x*x - 1);
+        dxdt[1] = z*x - y*(y*y - 2*x*x - 1);
+        dxdt[2] = z*(2*x*x - y*x + 1);
+
 
         //lorenz
         // dxdt[0] = 10.*(y - x);
         // dxdt[1] = x*(28. - z) - y;
         // dxdt[2] = x*y - (8./3.)*z;
     }
+
+    public:
+        // Define Equilibrium points! - do later...
+
 };
 
 
 
 
 int main(int argc, char** argv ){
+
+    /* #####################################################################################
+        LOCAL VARIABLE DECLARATIONS AND PROGRAM SETTINGS
+       #####################################################################################
+    */
     // Create SFML window instance
     sf::RenderWindow mainwin(sf::VideoMode(800,800), "NICE TITLE");
     sf::Clock Clock;
@@ -107,34 +161,52 @@ int main(int argc, char** argv ){
     // y       : matrix containing solutions for all N coordinates
     // stepper : numerical method used to integrate the ODE
     std::vector<double> x(3);
-    std::vector<double> t, xc, yc;
+    std::vector<double> t, xc, yc, inflx, infly;
     std::vector<std::vector<double> > y;
     boost::numeric::odeint::runge_kutta_dopri5<std::vector<double> > stepper;
     // boost::numeric::odeint::adams_bashforth<4,std::vector<double> > stepper;
     // boost::numeric::odeint::euler<std::vector<double> > stepper;
 
-    // Generate uniform random numbers in a circle centerd at a,b with radius r:
-    double a,b,r,tmax;
-    int n;
-    // a = 1.5/sqrt(6);
-    // b = sqrt(1 - (1.5*1.5/6));
-    // a=0;b=0;
-    // r = 1.;
-    cmdline_settings(argc, argv, &r, &tmax, &a, &b, &n);
-    generate_random_circle(xc,yc,a,b,r,tmax,n);
+
+/****************************************************************************
+ * 
+ *  DEFINE YOUR SPECIFIC INITAL CONDITIONS HERE, YOU ADD THEM TO THE VECTORS
+ *  AS I DID BELOW
+ * 
+ * **************************************************************************/
+
+    std::vector<double> xt,yt,zt;
+    xt.push_back(1/sqrt(2));
+    yt.push_back(1/sqrt(2));
+    zt.push_back(0.);
+
+    xt.push_back(-1/sqrt(2));
+    yt.push_back(1/sqrt(2));
+    zt.push_back(0.);
+    
+    xt.push_back(-1/sqrt(2));
+    yt.push_back(1/sqrt(2));
+    zt.push_back(1);
+
+    xt.push_back(1/sqrt(2));
+    yt.push_back(1/sqrt(2));
+    zt.push_back(1);
 
 
-    // preparing opengl surface. defining default window color.
-    // glClearDepth(1.f);
-    glClearColor(0.f, 0.f, 0.f, 0.f);
-    // glEnable(GL_DEPTH_TEST);
-    // glDepthMask(GL_TRUE);
+    xt.push_back(0);
+    yt.push_back(1);
+    zt.push_back(1e-5);
 
-    // setup perspective projection and the cameras position:
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(60.f, 1., 1.f, 5.); // fovy, aspect ratio, znear, zfar // objects farther than zfar or closer than znear will NOT be rendered.
-    glPointSize(5); // size of point sprites on the window. default is 1 pixel.
+    xt.push_back(1);
+    yt.push_back(0);
+    zt.push_back(1e-5);
+    
+
+    xt.push_back(-1);
+    yt.push_back(0);
+    zt.push_back(1e-5);
+    
+
 
     // Camera rotation flags and properties.
     bool rotate = false;
@@ -146,9 +218,73 @@ int main(int argc, char** argv ){
     float angley = 0;
     float anglez = 0;
     float rotspeed = 10;
-
+    // mainwin.setFramerateLimit(30);
     // Scalar field parameter.
     double lambda;
+
+
+
+    // Initial conditions, dynamical system settings, and gui settings for the window.
+    int numinit;
+    double r, tmax, xcenter, ycenter, sysstep;
+    float bkg_alpha, bkg_r, bkg_g, bkg_b, \
+          gui_aspect, gui_zfar, gui_znear, \
+          gui_fovy, gui_camera_dist;
+    std::string sysmethod, sysfile;
+
+
+    /* ######################################################################################
+        LOAD PROGRAM CONFIGURATION FROM CONFIG.INI!!!!
+    
+        need to capture variables from the config file, and assign them in this block of main.
+        #####################################################################################
+    */
+    boost::program_options::variables_map vm;
+    config_mapping(argc, argv, vm);
+
+    r = vm["init.conds.radius"].as<double>();
+    tmax = vm["init.conds.thetamax"].as<double>();
+    xcenter = vm["init.conds.xcenter"].as<double>();
+    ycenter = vm["init.conds.ycenter"].as<double>();
+    numinit = vm["init.conds.numinit"].as<int>();
+    sysfile = vm["system.file"].as<std::string>();
+    sysstep = vm["system.stepsize"].as<double>();
+    sysmethod = vm["system.method"].as<std::string>();
+    bkg_r = vm["gui.bkg_r"].as<float>();
+    bkg_g = vm["gui.bkg_g"].as<float>();
+    bkg_b = vm["gui.bkg_b"].as<float>();
+    bkg_alpha = vm["gui.bkg_alpha"].as<float>();
+    gui_fovy = vm["gui.fovy"].as<float>();
+    gui_aspect = vm["gui.aspect"].as<float>();
+    gui_znear = vm["gui.znear"].as<float>();
+    gui_zfar = vm["gui.zfar"].as<float>();
+    gui_camera_dist = vm["gui.camera_dist"].as<float>();
+
+
+    // Generate uniform random numbers in a circle centerd at a,b with radius r:
+    generate_random_circle(xc,yc,xcenter,ycenter,r,tmax,numinit);
+
+
+
+
+
+
+
+    // preparing opengl surface. defining default window color.
+    glClearDepth(1.f);
+    glClearColor(bkg_r, bkg_g, bkg_b, bkg_alpha);
+    glEnable(GL_DEPTH_TEST);
+    glDepthMask(GL_TRUE);
+    glEnable(GL_BLEND);
+    glHint(GL_LINE_SMOOTH, GL_NICEST);
+
+    // setup perspective projection and the cameras position:
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(gui_fovy, gui_aspect, gui_znear, gui_zfar); // fovy, aspect ratio, znear, zfar // objects farther than zfar or closer than znear will NOT be rendered.
+    glPointSize(5); // size of point sprites on the window. default is 1 pixel.
+    // glRotatef(90.,1.,0.,0.);
+
 
     // Drawing circles around the axis for spatial reference.
     std::vector<float> xcirc,ycirc;
@@ -159,10 +295,11 @@ int main(int argc, char** argv ){
         ycirc.push_back(sin(th));
     }
 
+    glutInit(&argc, argv);
 
     // SFML main window instance. Drawing handled in pure opengl context.
     while (mainwin.isOpen()) {
-        for (int q = 0; q < 1; q++) {
+        for (int q = 0; q < 250; q++) {
 
             // Handle events through the SFML interface for the window. (keyboard press, closing, etc.)
             sf::Event event;
@@ -208,15 +345,35 @@ int main(int argc, char** argv ){
 			    }
                 if ((event.type == sf::Event::KeyPressed) && (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad6))){
                     anglez -= 0.1; // hold down negative rotation in z
-			    }
+			    } 
+                if (event.type == sf::Event::Resized)
+                {
+                    // adjust the viewport when the window is resized, adjusting
+                    // the aspect ratio so we don't stretch the image.
+                    float new_aspect = (float)mainwin.getSize().x/(float)mainwin.getSize().y;
+                    // Adjusts the size of the viewport to reflect window resizing.
+                    glViewport(0, 0, event.size.width, event.size.height);
+                    // this is old opengl, switch to projection matrix mode,
+                    // load identity so we dont stack projections by mistake,
+                    // then change our perspective to the new aspect ratio
+                    // then finalize by switching back to modelview matrix mode
+                    // so that the drawings arent fucked up after.
+                    glMatrixMode(GL_PROJECTION);
+                    glLoadIdentity();
+                    gluPerspective(gui_fovy, new_aspect, gui_znear, gui_zfar);
+                    glMatrixMode(GL_MODELVIEW);
+                }
             } // event loop
 
             // clear window to prepare for drawing. fill window background with default color (defined above)
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             glMatrixMode(GL_MODELVIEW);
             glLoadIdentity();
-            glTranslatef(0.f,0.f,-2.f);
-            
+            glTranslatef(0.f,0.f,-gui_camera_dist);
+            glRotatef(-90.,1.,0.,0.); // this rotates to put XY plane into perspective
+
+            // glColor4f(1,1,1,0.);
+            // glutSolidSphere(1,50,50);
             // perspective transformations... rotate the coordinates if toggles are on.
             if (rotatex) {
                 anglex += .05;
@@ -235,41 +392,48 @@ int main(int argc, char** argv ){
             // numerical solutions...
             double ystart = 0.01; // value for initial conditions. Will change to random seeded values later?
             double xp, yp, zp; // temporary variables for holding numbers. These are (probably) removed with -O3 optimization
-            lambda = 10; // scalar field parameter. can be fixed or change through the q loop. if fixed, q loop will be optimized away.
-            
+            lambda = 0.01*q; // scalar field parameter. can be fixed or change through the q loop. if fixed, q loop will be optimized away.
+    
             // Numerically solve and draw lines...
             for (int j = 0; j < xc.size(); j++) {
                 
                 // System to solve!
-                sean_problem test(lambda); 
+                dynamical_system test(lambda); 
 
-                //forward solution
-                // x[0] = 0.01*j; x[1] = 0.01*j; x[2] = 0.01*j;// generic initial conditions
-                // x[0] = -x_start(ystart) + 2*x_start(ystart)*j/20; x[2] = ystart; x[1] = 0.01; // XZ plane solution initial conditions (same as 2D 'simple' solution)
-                x[0] = xc[j]; x[1] = 0.01; x[2] = yc[j];
-                boost::numeric::odeint::integrate_const(stepper,test, x, 0.,10.,0.01,push_back_state_and_time(y,t)); // forward solution
+
+
+                //FORWARD TIME INTEGRATION.
+                // INITIAL CONDITIONS SET FROM VECTORS DEFINED ABOVE
+                // x[0] = xc[j]; x[1] = yc[j]; x[2] = 0.5; // This line defines randomly generated initial conditions 
+                x[0] = xt[j]; x[1] = yt[j]; x[2] = zt[j];
+                boost::numeric::odeint::integrate_const(stepper,test, x, 0.,10.,0.01,push_back_state_and_time(y,t));
+
+                // DRAWS FORWARD SOLUTION
                 glBegin(GL_LINE_STRIP);
                     for (int i = 0; i < y.size(); i++) {
-                        glColor3f(0,1,0);
-                        xp = y[i][0]; yp = y[i][1]; zp = y[i][2];
+                        xp = y[i][0]; yp = y[i][1]; zp = y[i][2]; //placeholders for readability
+                        glColor3f(0.4,0,0.8);
                         glVertex3f(xp, yp, zp);
                     }
                 glEnd();
                 y.clear(); t.clear();
-                
-                // backward solution
-                // x[0] = 0.01*j; x[1] = 0.01*j; x[2] = 0.01*j;
-                // x[0] = -x_start(ystart) + 2*x_start(ystart)*j/20; x[2] = ystart; x[1] = 0.01; // XZ plane solution conditions.
-                x[0] = xc[j]; x[1] = 0.01; x[2] = yc[j];
-                boost::numeric::odeint::integrate_const(stepper,test, x, 10.,0.,-0.01,push_back_state_and_time(y,t)); // forward solution
 
+
+                // DRAWS BACKWARD SOLUTION
+                boost::numeric::odeint::integrate_const(stepper,test, x, 10.,0.,-0.01,push_back_state_and_time(y,t));
+                
                 // Draw solution curves in 3D phase space.
                 glBegin(GL_LINE_STRIP);
                     for (int i = 0; i < y.size(); i++) {
-                        glColor3f(0,1,0);
-                        xp = y[i][0]; yp = y[i][1]; zp = y[i][2];
+                        xp = y[i][0]; yp = y[i][1]; zp = y[i][2]; //placeholders for readability
+                        glColor3f(0.4,0,0.8);
                         glVertex3f(xp, yp, zp);
                     }
+                glEnd();
+
+                glBegin(GL_POINTS);
+                glColor3f(1,0,0);
+                glVertex3f(xt[j],yt[j],zt[j]);
                 glEnd();
 
                 // delete current solution curve to prepare for the next one...
@@ -297,24 +461,22 @@ int main(int argc, char** argv ){
                 }
             glEnd();
 
+
             // DRAW EQUILIBRIUM POINTS
             // these floats are for the non trivial eq points.
-            float fepx = -lambda/sqrt(6.);
-            float fepz = sqrt(1 - lambda*lambda/6.);
-            float hepx = sqrt(6.)/(3.*lambda);
-            float hepz = 2./(sqrt(3.)*lambda); 
+            // float fepx = lambda/sqrt(6.);
+            // float fepz = sqrt(1 - lambda*lambda/6.);
+            // float hepx = sqrt(6.)/(3.*lambda);
+            // float hepz = 2./(sqrt(3.)*lambda); 
             glBegin(GL_POINTS);
-                glColor3f(1,1,1);
-                glVertex3f(0,0,0);
-                glVertex3f(1,0,0);
-                glVertex3f(-1,0,0);
-                glVertex3f(0,1,0);
-                glVertex3f(0,-1,0);
-                glVertex3f(fepx,0,fepz);
-                glVertex3f(hepx,0,hepz);
-                for (int i =0; i < xc.size(); i++) {
-                    glVertex3f(xc[i],0,yc[i]);
-                }
+                // glColor3f(1,0,1);
+                // glVertex3f(0,0,0);
+                // glVertex3f(1,0,0);
+                // glVertex3f(-1,0,0);
+                // glVertex3f(0,1,0);
+                // glVertex3f(0,-1,0);
+                // glVertex3f(fepx,0,fepz);
+                // glVertex3f(hepx,0,hepz);
             glEnd();
 
             // Display everything we've done to the SFML instance
